f9be9dee816d7184aa4d242e500c28e7a946fefe
Rick Duel <Rickduel@gmail.com>
2021-04-02 14:41:54 -0400

    DL

---
+++ macros/arrow_animation_rand.js
@@ -0,0 +1,104 @@
+//This macro plays the animation on selected targets with a trajectory and distances of 30ft, 60ft and 90ft
+//And We play another animation on top of that on impact. Perfect for Explosive Arrows !
+//folder 01 is the directory path to the Arrow assets and folder02 is for the Explosion
+
+let folder01 = "https://assets.forge-vtt.com/bazaar/modules/jb2a_patreon-2d960ceba80cc778/assets/Library/Generic/Weapon_Attacks/Ranged/";
+let folder02 = "modules/jb2a_patreon/Library/Generic/Explosion/"
+// anFile30 points to the file corresponding to 30ft, anFile60 for 60ft and anFile90 for 90ft
+//anFileXpl is for the file of the explosion.
+let anFile30 = `${folder01}Arrow_Simple_01_30ft_1600x400.webm`;
+let anFile60 = `${folder01}Arrow_Simple_01_60ft_2800x400.webm`;
+let anFile90 = `${folder01}Arrow_Simple_01_90ft_4000x400.webm`;
+let anFileXpl = `${folder02}Explosion_01_Orange_400x400.webm`;
+
+
+//if(game.user.targets.size == 0) ui.notifications.error('You must target at least one token');
+//if(canvas.tokens.controlled.length == 0) ui.notifications.error("Please select your token");
+///Check if Module dependencies are installed or returns an error to the user
+if (!canvas.fxmaster) ui.notifications.error("This macro depends on the FXMaster module. Make sure it is installed and enabled");
+
+const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
+
+async function Cast() {
+var myStringArray = Array.from(game.user.targets)[0];
+var arrayLength = game.user.targets.size;
+for (var i = 0; i < arrayLength; i++) {
+
+let mainTarget = Array.from(game.user.targets)[i];
+let tarScale = ((mainTarget.data.width + mainTarget.data.height)/2);
+let myToken = canvas.tokens.controlled [0];
+if (myToken == null){myToken = canvas.tokens.ownedTokens[0];} 
+
+let ray = new Ray(myToken.center, mainTarget.center);
+let anDeg = -(ray.angle * 57.3);
+let anDist = ray.distance;
+anDeg += (Math.random()-.5)*3*tarScale;
+anDist+= (Math.random()-.5)*50*tarScale;
+
+
+
+let anFile = anFile30;
+let anFileSize = 600;
+let anchorX = 0.125;
+switch(true){
+ case (anDist<=1200):
+    anFileSize = 1200;
+    anFile = anFile30;
+    anchorX = 0.125;
+    break;
+ case (anDist>2400):
+    anFileSize = 3600;
+    anFile = anFile90;
+    anchorX = 0.05;
+    break;
+ default:
+    anFileSize = 2400;
+    anFile = anFile60;
+    anchorX = 0.071;
+    break;
+}
+
+let anScale = anDist / anFileSize;
+let anScaleY = anDist <= 600 ? 0.6  : anScale;
+
+let spellAnim = 
+                    {
+                     file: anFile,
+                      position: myToken.center,
+                      anchor: {
+                       x: anchorX,
+                       y: 0.5
+                      },
+                      angle: anDeg,
+                      scale: {
+                       x: anScale,
+                       y: anScaleY
+                      }
+                    }; 
+
+let spellAnim2 = 
+                    {
+                     file: anFileXpl,
+                      position: mainTarget.center,
+                      anchor: {
+                       x: 0.5,
+                       y: 0.5
+                      },
+                      angle: 0,
+                      scale: {
+                       x: tarScale,
+                       y: tarScale
+                      }
+                    }; 
+
+canvas.fxmaster.playVideo(spellAnim);
+await sleepNow(150);
+
+
+
+//canvas.fxmaster.playVideo(spellAnim2);
+//await sleepNow(250);
+game.socket.emit('module.fxmaster', spellAnim);
+}
+}
+Cast ()
--- test.js
+++ test.js
@@ -1,79 +1,102 @@
+const lastArg = args[args.length - 1];
+let tactor;
+if (lastArg.tokenId) tactor = canvas.tokens.get(lastArg.tokenId).actor;
+else tactor = game.actors.get(lastArg.actorId);
+
+
 if (args[0] === "on") {
-async function dropManyTokens(actorName, number){
-    if(canvas.tokens.controlled.length == 0) {ui.notifications.error("Please select a token");
-return;
-}
-let myToken = canvas.tokens.controlled [0];
-    function randInt(min, max){
-      return Math.floor(Math.random() * (max - min)) + min
+    async function dropManyTokens(actorName, number){
+        if(canvas.tokens.controlled.length == 0) {ui.notifications.error("Please select a token");
+    return;
     }
-    
-    function pos(){
-        
-        let pos = [myToken.position.x, myToken.position.y];
-        return pos;
-       
-    }
-    
-    
-    function convertCoord (square) {
-      return [
-        ( square[0] + 0.5 ) * canvas.grid.w,
-        canvas.grid.height - (( square[1] + 0.5 ) * canvas.grid.h),
-        ];
-    }
-  
-    const actor = game.actors.getName(actorName);
-   
-  
-    for (var i = 0; i < number; i++){
-        console.log(pos());
-        var xOff = pos()[0];
-        var yOff = pos()[1];
-        switch (i) {
-            case 0:
-                 xOff = pos()[0]+canvas.grid.size;
-        break;
-
-        case 1:
-             xOff = pos()[0]-canvas.grid.size;
-           break;
-
-           case 2:
-             yOff = pos()[1]+canvas.grid.size;
-        break;
-
-        case 3:
-             yOff = pos()[1]-canvas.grid.size;
-        break;
+    let myToken = canvas.tokens.controlled [0];
+        function randInt(min, max){
+          return Math.floor(Math.random() * (max - min)) + min
         }
-
-
-        // let xOff = pos()[0]+canvas.grid.size;
-        // let yOff = pos()[1]+canvas.grid.size;
         
-
-        const extraTokenData = {
-        x: xOff,
-        y: yOff,
-      };
-      const token = await Token.fromActor(actor, extraTokenData);
-      await Token.create(token.data);
+        function pos(){
+            
+            let pos = [myToken.position.x, myToken.position.y];
+            return pos;
+           
+        }
+        
+        
+        function convertCoord (square) {
+          return [
+            ( square[0] + 0.5 ) * canvas.grid.w,
+            canvas.grid.height - (( square[1] + 0.5 ) * canvas.grid.h),
+            ];
+        }
+      
+        const actor = game.actors.getName(actorName);
+       
+      
+        for (var i = 0; i < number; i++){
+            console.log(pos());
+            var xOff = pos()[0];
+            var yOff = pos()[1];
+            switch (i) {
+                case 0:
+                     xOff = pos()[0]+canvas.grid.size;
+            break;
+    
+            case 1:
+                 xOff = pos()[0]-canvas.grid.size;
+               break;
+    
+               case 2:
+                 yOff = pos()[1]+canvas.grid.size;
+            break;
+    
+            case 3:
+                 yOff = pos()[1]-canvas.grid.size;
+            break;
+            }
+    
+    
+            // let xOff = pos()[0]+canvas.grid.size;
+            // let yOff = pos()[1]+canvas.grid.size;
+            
+    
+            const extraTokenData = {
+            x: xOff,
+            y: yOff,
+            flags: {
+                DL: {
+                  DancingLights:{
+                      ActorId: tactor.id
+                }
+              }
+          },
+        }
+          const token = await Token.fromActor(actor, extraTokenData);
+          await Token.create(token.data);
+        }
+        
+    }
+        
+        dropManyTokens("Dancing light - Wildcard", 4);
     }
     
-}
+
+
+
+
+    if (args[0] === "off") {
+       
+        let dlToks = canvas.tokens.placeables.filter(i => i.data.flags?.DL?.DancingLights?.ActorId === tactor.id)
+        var dlToksID =[];
+        var i=0;
+        for (i=0; i < dlToks.length; i++){
+
+            dlToksID.push(dlToks[i].data._id);
+
+        }
+        
+        await canvas.tokens.deleteMany(dlToksID)
+
+
     
-    dropManyTokens("Dancing light - Wildcard", 4);
-}
-
-if (args[0] === "off") {
-
-    let token = canvas.scene.data.tokens.find(token => token.name = 'Dancing light - Wildcard');
-    canvas.tokens.deleteMany(token.id);
-
-
-
-
-
-
-}
+    }
+    
