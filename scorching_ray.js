495cf54904ca0f991fd9b5e68d278abd2735f012
Rick Duel <Rickduel@gmail.com>
2021-04-03 03:43:34 -0400

    Move Template

--- scorching_ray.js
+++ scorching_ray.js
@@ -5,86 +5,19 @@ let tactor;
 if (lastArg.tokenId) tactor = canvas.tokens.get(lastArg.tokenId).actor;
 else tactor = game.actors.get(lastArg.actorId);
 
-const DAEItem = lastArg.efData.flags.dae.itemData
-const saveData = DAEItem.data.save
-const DC = args[1]
+
+const spellLevel = args[1]
 /**
  * Create Moonbeam item in inventory
  */
 if (args[0] === "on") {
-  let templateData = {
-    t: "circle",
-    user: game.user._id,
-    distance: 5,
-    direction: 0,
-    x: 0,
-    y: 0,
-    flags: {
-        DAESRD: {
-            Moonbeam: {
-                ActorId: tactor.id
-            }
-        }
-    },
-    fillColor: game.user.color
-}
-let template = new game.dnd5e.canvas.AbilityTemplate(templateData)
-template.actorSheet = tactor.sheet;
-template.drawPreview()
+ 
 
-  let damage = DAEItem.data.level;
+
+  
   await tactor.createOwnedItem(
     {
-      "name": "Moonbeam repeating",
-      "type": "spell",
-      "data": {
-        "source": "Casting Moonbeam",
-        "ability": "",
-        "description": {
-          "value": "half damage on save" 
-        },
-        "actionType": "save",
-        "attackBonus": 0,
-        "damage": {
-          "parts": [
-            [
-              `${damage}d10`,
-              "radiant"
-            ]
-          ],
-        },
-        "formula": "",
-        "save": {
-          "ability": "con",
-          "dc": saveData.dc,
-          "scaling": "spell"
-        },
-        "level": 0,
-        "school": "abj",
-        "preparation": {
-          "mode": "prepared",
-          "prepared": false
-        },
-
-      },
-      "img": DAEItem.img,
-    }
-  );
-}
-
-// Delete Moonbeam
-if (args[0] === "off") {
-  let casterItem = tactor.data.items.find(i => i.name === "Moonbeam repeating" && i.type === "spell")
-  tactor.deleteOwnedItem(casterItem._id)
-  let template = canvas.templates.placeables.filter(i => i.data.flags.DAESRD?.Moonbeam?.ActorId === tactor.id)
-    canvas.templates.deleteMany(template[0].id)
-}
-
-
-let tactor = canvas.tokens.controlled[0];
-let spellLevel = 2;
-await tactor.actor.createOwnedItem( 
-    { "name": "Scorching Ray EX",
+      "name": "Scorching Ray EX",
   "type": "spell",
   "data": {
     "description": {
@@ -200,3 +133,11 @@ await tactor.actor.createOwnedItem(
   "effects": []
 }
 );
+}
+
+// Delete Moonbeam
+if (args[0] === "off") {
+ debugger;
+  let casterItem = tactor.data.items.find(i => i.name === "Scorching Ray (Extra Attacks)" && i.type === "spell")
+  tactor.deleteOwnedItem(casterItem._id)
+}
